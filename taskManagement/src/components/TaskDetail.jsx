7a0a921ca32ec302f81d126d9f415a6e6565d361
Shobana Christy <shobichristy07@gmail.com>
2025-11-02 11:18:30 -0600

    practiced task management

---
+++ taskManagement/src/components/TaskDetail.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useParams, Link } from "react-router";
+import { tasks } from "./tasks";
+
+
+
+const TaskDetail = () => {
+    const { id } = useParams();
+
+    const task = tasks.find((task) => task.id == parseInt(id));
+
+    return (
+        <div
+    style={{
+            background: "white",
+            margin: "8px auto",
+            padding: "12px",
+            width: "300px",
+            borderRadius: "8px",
+            boxShadow:" 0px 2px 5px rgba(0, 0, 0, 0.1)",
+            cursor: "pointer",
+            transition:" all 0.3s ease"}}
+    >
+            <h2>TaskDetail</h2>
+
+            {task ? (
+                <>
+                    <h3>{task.title}</h3>
+                    <p> {task.description}</p>
+                </>) :
+                (<p>Task Does not exist</p>)
+            }
+            <Link to={"/"}>Back to Main </Link>
+        </div>
+    )
+}
+
+export default TaskDetail;
---
+++ taskManagement/src/components/TaskList.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router";
+import { tasks } from "./tasks";
+
+
+const TaskList = () => {
+
+    return (
+        <div>
+            <h1>Task List</h1>
+            <ol style={{
+            background: "white",
+            margin: "8px auto",
+            padding: "12px",
+            width: "300px",
+            borderRadius: "8px",
+            boxShadow:" 0px 2px 5px rgba(0, 0, 0, 0.1)",
+            cursor: "pointer",
+            transition:" all 0.3s ease"}}>
+            
+                {tasks.map((task) => (
+                    <li key={task.id}>
+                        <Link to={`/task/${task.id}`}> {/* Link navigates to /task/${task.id}*/}
+                            {task.title} {/* Displays tasks list with title */}
+                        </Link>
+                    </li>
+                ))}
+           </ol>
+
+        </div>
+    )
+
+}
+
+export default TaskList;
